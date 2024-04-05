7915aafa32a6c531918afbbe95f7034e7612bc3d
pyshkovni <guess.the.tutor@gmail.com>
2024-04-05 13:49:53 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# Этап 1
+
+# Дополнительный гайд по aiogram https://mastergroosha.github.io/aiogram-3-guide/quickstart/
+# Дополнительный гайд по асинхронному бэкенду https://habr.com/ru/companies/kts/articles/598575/
+# Дополнительный гайд по логированию https://habr.com/ru/companies/wunderfund/articles/683880/
+
+# Установить библиотеки и фреймворки aiogram==3.4
+# pip install aiogram
+# pip install python-dotenv
+
+# Для сохранения текущих пакетов в проекте и их версий при переносе используйте файл requirements.txt
+# pip freeze — внешние пакеты проекта
+# pip freeze > requirements.txt — сохранить внешние пакеты в файл
+# pip install -r requirements.txt —  загрузить пакеты из файла
+
+
+# импорты
