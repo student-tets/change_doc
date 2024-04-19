28ba61afdb545caeb6af3a74898f2f6bfa829b93
pyshkovni <guess.the.tutor@gmail.com>
2024-04-19 12:53:16 +0300

    Дополнили config.py переменными окружения dotenv

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
 # переменные окружения
-TOKEN = "ВАШ_ТОКЕН"
+# python-dotenv нужен для настройки переменных окружения
+
+# Для передачи токена создайте файл .env со следующим содержимым
+# TOKEN=ВАШ_ТОКЕН
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN: str = os.getenv('TOKEN')
+
