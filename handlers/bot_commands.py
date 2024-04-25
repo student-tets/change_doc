0b5328b85351fc944e2d5101861b01d743ff9f96
pyshkovni <guess.the.tutor@gmail.com>
2024-04-25 11:03:53 +0300

    создали список команд бота с описанием для передачи в main

--- handlers/bot_commands.py
+++ handlers/bot_commands.py
@@ -1,3 +1,14 @@
 __all__ = [
-
+    "commands_for_bot",
 ]
+
+from aiogram import types
+
+bot_commands = (
+    ("help", "Справка по боту"),
+    # TODO ("status", "Показать статус пользователя"),
+)
+
+commands_for_bot = []
+for cmd in bot_commands:
+    commands_for_bot.append(types.BotCommand(command=cmd[0], description=cmd[1]))
