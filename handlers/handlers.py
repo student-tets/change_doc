d5f27e709849a9f5323637a152e6188f53e961fb
pyshkovni <guess.the.tutor@gmail.com>
2024-04-25 10:37:18 +0300

    описали подключение к базе данных их нового хендлера help_command

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -2,16 +2,38 @@ import logging
 from aiogram import Router
 from aiogram import types
 from aiogram.filters.command import Command
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+from db import async_session_maker, User
+
 
 # настройка логирования
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-# @dp.message()
-async def process_start_command(message: types.Message):
-    """Приветствие"""
-    await message.answer("Привет!\nНапиши что-нибудь")
-    logging.info(f'user {message.from_user.id} starts bot')
+
+async def help_command(message: types.Message):
+    """справочная команда, регистрация пользователя"""
+
+    async with async_session_maker() as session:
+        session: AsyncSession
+        query = select(User).where(User.user_id == message.from_user.id)
+        user_exit = await session.execute(query)
+
+        if user_exit.scalars().all():
+            await message.reply("Ты есть!")
+            logging.info(f"user {message.from_user.id} asks for help")
+
+        else:
+            new_user = {
+                "user_id": message.from_user.id,
+                "username": message.from_user.username
+            }
+            stmt = insert(User).values(**new_user)
+            await session.execute(stmt)
+            await session.commit()
+            await message.reply("Теперь ты есть!")
+            logging.info(f"register new user: {message.from_user.id}")
 
 
 # @dp.message()
@@ -22,5 +44,5 @@ async def echo_command(message: types.Message):
 
 def register_message_handler(router: Router):
     """Маршрутизация"""
-    router.message.register(process_start_command, Command(commands=['start']))
+    router.message.register(process_start_command, Command(commands=["start", "help"]))
     router.message.register(echo_command)
